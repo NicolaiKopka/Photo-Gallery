a9d0164f2250ea674f9ab14fb6d111aeaa0e6908
Nicolai Kopka <nicolai.kopka@aol.de>
2022-09-12 15:40:16 +0200

    hide id

--- frontend/src/api_methods.ts
+++ frontend/src/api_methods.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export function uploadToCloudinary(formData: FormData) {
-    return axios.post("https://api.cloudinary.com/v1_1/drzar88fa/image/upload", formData)
+    return axios.post("https://api.cloudinary.com/v1_1/cloudinary_id/image/upload", formData)
         .then(response => response.data)
 }
